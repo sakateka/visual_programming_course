aa0f9f09d58c6f240be3985bdad90b00077d53ae
Sergey Kacheev <s.kacheev@gmail.com>
2017-11-12 23:06:16 -0800

    xmm.. unit2 lab1 complete

--- lab1/Unit1.dfm
+++ lab1/Unit1.dfm
@@ -2,7 +2,7 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 299
+  ClientHeight = 302
   ClientWidth = 632
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -19,51 +19,32 @@ object Form1: TForm1
     Left = 257
     Top = 41
     Width = 375
-    Height = 215
+    Height = 242
     Align = alClient
     ExplicitWidth = 16
     ExplicitHeight = 218
-  end
-  object Panel1: TPanel
-    Left = 0
-    Top = 256
-    Width = 632
-    Height = 43
-    Margins.Left = 5
-    Margins.Top = 5
-    Margins.Right = 5
-    Margins.Bottom = 5
-    Align = alBottom
-    Alignment = taRightJustify
-    TabOrder = 0
-    object Label1: TLabel
-      Left = 16
-      Top = 14
-      Width = 253
-      Height = 13
-      Caption = 'C:\Program Files (x86)\Embarcadero\Studio\19.0\bin'
-    end
   end
   object DirectoryListBox1: TDirectoryListBox
     Left = 0
     Top = 41
     Width = 257
-    Height = 215
+    Height = 242
     Align = alLeft
-    DirLabel = Label1
     FileList = FileListBox1
-    TabOrder = 1
+    TabOrder = 0
     OnChange = DirectoryListBox1Change
+    ExplicitHeight = 215
   end
   object FileListBox1: TFileListBox
     Left = 257
     Top = 41
     Width = 375
-    Height = 215
+    Height = 242
     Align = alClient
     ItemHeight = 13
-    TabOrder = 2
+    TabOrder = 1
     OnChange = FileListBox1Change
+    ExplicitHeight = 215
   end
   object Panel2: TPanel
     Left = 0
@@ -71,7 +52,7 @@ object Form1: TForm1
     Width = 632
     Height = 41
     Align = alTop
-    TabOrder = 3
+    TabOrder = 2
     object DriveComboBox1: TDriveComboBox
       Left = 16
       Top = 11
@@ -88,5 +69,30 @@ object Form1: TForm1
       FileList = FileListBox1
       TabOrder = 1
     end
+  end
+  object StatusBar1: TStatusBar
+    Left = 0
+    Top = 283
+    Width = 632
+    Height = 19
+    DoubleBuffered = False
+    Panels = <
+      item
+        Bevel = pbRaised
+        Width = 300
+      end
+      item
+        Bevel = pbRaised
+        Width = 50
+      end
+      item
+        Alignment = taRightJustify
+        Bevel = pbNone
+        BiDiMode = bdLeftToRight
+        ParentBiDiMode = False
+        Width = 25
+      end>
+    ParentDoubleBuffered = False
+    ExplicitTop = 289
   end
 end
--- lab1/Unit1.pas
+++ lab1/Unit1.pas
@@ -5,21 +5,23 @@ interface
 uses
   Winapi.Windows, Winapi.Messages,
   System.SysUtils, System.Variants, System.Classes, System.Generics.Collections,
-  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.FileCtrl, Vcl.ExtCtrls;
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.FileCtrl, Vcl.ExtCtrls,
+  Vcl.ComCtrls;
 
 type
   TForm1 = class(TForm)
-    Panel1: TPanel;
     DirectoryListBox1: TDirectoryListBox;
     Splitter1: TSplitter;
     FileListBox1: TFileListBox;
     Panel2: TPanel;
     DriveComboBox1: TDriveComboBox;
     FilterComboBox1: TFilterComboBox;
-    Label1: TLabel;
+    StatusBar1: TStatusBar;
     procedure FormCreate(Sender: TObject);
     procedure FileListBox1Change(Sender: TObject);
     procedure DirectoryListBox1Change(Sender: TObject);
+    procedure SetStatusBarTextAndWidth(textP1, textP2: String);
+    procedure SetFileFilter();
   private
     { Private declarations }
   public
@@ -33,7 +35,59 @@ implementation
 
 {$R *.dfm}
 
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  SetFileFilter;
+  DirectoryListBox1.Align := alLeft;
+  DirectoryListBox1.Width := Form1.ClientWidth div 3;
+  Splitter1.Left := DirectoryListBox1.Left + DirectoryListBox1.Width + 1;
+  Splitter1.Align := DirectoryListBox1.Align;
+  Splitter1.Width := 4;
+  Splitter1.MinSize := Form1.ClientWidth div 4;
+  DirectoryListBox1.FileList := FileListBox1;
+  SetStatusBarTextAndWidth(DirectoryListBox1.Directory, '');
+end;
+
 procedure TForm1.DirectoryListBox1Change(Sender: TObject);
+begin
+  SetFileFilter;
+  SetStatusBarTextAndWidth(DirectoryListBox1.Directory, '');
+end;
+
+procedure TForm1.FileListBox1Change(Sender: TObject);
+
+begin
+  if FileListBox1.ItemIndex >= 0 then
+    SetStatusBarTextAndWidth(DirectoryListBox1.Directory, FileListBox1.Items[FileListBox1.ItemIndex]);
+
+end;
+
+procedure TForm1.SetStatusBarTextAndWidth(textP1, textP2: String);
+  var F: TSearchRec;
+      PanelWidth: Integer;
+      formattedTime: String;
+begin
+  StatusBar1.Canvas.Font := StatusBar1.Font;
+  DateTimeToString(formattedTime, '  hh:nn:ss      ', Time);
+  StatusBar1.Panels[2].Text := formattedTime;
+  StatusBar1.Panels[2].Width := StatusBar1.Canvas.TextWidth(StatusBar1.Panels[2].Text);
+  PanelWidth := StatusBar1.Width - StatusBar1.Panels[2].Width;
+
+
+  if textP2.Length > 0 then
+    if FindFirst(textP2, faAnyFile, F) = 0 then
+       textP2 := F.Name + ' ' + IntToStr(F.Size) + ' bytes';
+  StatusBar1.Panels[1].Text :=  textP2;
+  StatusBar1.Panels[1].Width := StatusBar1.Canvas.TextWidth(textP2) + 6;
+
+  PanelWidth := PanelWidth - StatusBar1.Panels[1].Width;
+  if (StatusBar1.Canvas.TextWidth(textP1) + 6) > PanelWidth then
+    textP1 := MinimizeName(textP1, StatusBar1.Canvas, PanelWidth - 6);
+  StatusBar1.Panels[0].Text := textP1;
+  StatusBar1.Panels[0].Width := PanelWidth;
+end;
+
+procedure TForm1.SetFileFilter();
   var dict: TDictionary<String, boolean>;
   ext: String;
   FName: String;
@@ -48,24 +102,4 @@ begin
   for ext in dict.Keys do
     FilterComboBox1.Filter :=  FilterComboBox1.Filter + '|' + '*' + ext + '|' + '*' + ext;
 end;
-
-procedure TForm1.FileListBox1Change(Sender: TObject);
-  var F: TSearchRec;
-begin
-  if FileListBox1.ItemIndex >= 0 then
-    if FindFirst(FileListBox1.Items[FileListBox1.ItemIndex], faAnyFile, F) = 0 then
-      Panel1.Caption := F.Name + ' ' + IntToStr(F.Size) + ' bytes ';
-end;
-
-procedure TForm1.FormCreate(Sender: TObject);
-begin
-  DirectoryListBox1.Align := alLeft;
-  DirectoryListBox1.Width := Form1.ClientWidth div 3;
-  Splitter1.Left := DirectoryListBox1.Left + DirectoryListBox1.Width + 1;
-  Splitter1.Align := DirectoryListBox1.Align;
-  Splitter1.Width := 4;
-  Splitter1.MinSize := Form1.ClientWidth div 4;
-  DirectoryListBox1.FileList := FileListBox1;
-end;
-
 end.
