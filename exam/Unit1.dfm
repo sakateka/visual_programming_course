6d4b0698cbb968eb4feda363ac91166633ac4072
sakateka <S.Kacheev@gmail.com>
2017-11-29 23:16:57 +0700

    exam: complete unit1, init unit2

---
+++ exam/Unit1.dfm
@@ -0,0 +1,81 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 310
+  ClientWidth = 625
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  Menu = MainMenu1
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 8
+    Top = 5
+    Width = 5
+    Height = 19
+    Color = clRed
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clRed
+    Font.Height = -16
+    Font.Name = 'Tahoma'
+    Font.Style = [fsBold]
+    ParentColor = False
+    ParentFont = False
+  end
+  object StringGrid1: TStringGrid
+    Left = 8
+    Top = 30
+    Width = 609
+    Height = 219
+    FixedCols = 0
+    FixedRows = 0
+    TabOrder = 0
+    Visible = False
+  end
+  object StringGrid2: TStringGrid
+    Left = 8
+    Top = 263
+    Width = 609
+    Height = 33
+    FixedCols = 0
+    RowCount = 1
+    FixedRows = 0
+    TabOrder = 1
+    Visible = False
+  end
+  object Button1: TButton
+    Left = 536
+    Top = 2
+    Width = 75
+    Height = 25
+    Caption = #1047#1072#1076#1072#1085#1080#1077' '#8470'2'
+    TabOrder = 2
+    OnClick = Button1Click
+  end
+  object MainMenu1: TMainMenu
+    Left = 576
+    Top = 264
+    object Start1: TMenuItem
+      Caption = 'Start'
+      OnClick = Start1Click
+    end
+    object N1: TMenuItem
+      Caption = #1057#1090#1088#1086#1082#1080
+      OnClick = N1Click
+    end
+    object N2: TMenuItem
+      Caption = #1057#1090#1086#1083#1073#1094#1099
+      OnClick = N2Click
+    end
+    object Minimum1: TMenuItem
+      Caption = 'Minimum'
+      OnClick = Minimum1Click
+    end
+  end
+end
---
+++ exam/Unit1.pas
@@ -0,0 +1,118 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Grids, Vcl.Menus, Vcl.StdCtrls, System.Math, Unit2;
+
+type
+  TForm1 = class(TForm)
+    StringGrid1: TStringGrid;
+    MainMenu1: TMainMenu;
+    Start1: TMenuItem;
+    N1: TMenuItem;
+    N2: TMenuItem;
+    Minimum1: TMenuItem;
+    StringGrid2: TStringGrid;
+    Label1: TLabel;
+    Button1: TButton;
+    procedure Start1Click(Sender: TObject);
+    procedure N1Click(Sender: TObject);
+    procedure N2Click(Sender: TObject);
+    procedure Minimum1Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  Mas: array of array of Extended;
+  D: array of Extended;
+  Rows: Integer;
+  Cols: Integer;
+
+const
+  RoundTo: Integer = 5000;
+
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Form2;
+end;
+
+procedure TForm1.Minimum1Click(Sender: TObject);
+var c: Integer;
+begin
+  if (StringGrid1.ColCount > 0) and (StringGrid1.RowCount > 0) then
+  begin
+    SetLength(D, StringGrid1.ColCount);
+    StringGrid2.ColCount := StringGrid1.ColCount;
+    for c := 0 to StringGrid1.ColCount -1 do
+    begin
+      D[c] := MinValue(Mas[c]);
+      StringGrid2.Cells[c, 0] := IntToStr(Round(D[c]*RoundTo));
+    end;
+    StringGrid2.Visible := true;
+  end;
+end;
+
+procedure TForm1.N1Click(Sender: TObject);
+var strRows: String;
+begin
+  strRows := inputbox('Количество строк', 'Укажите количество строк: ', '0');
+  Rows := StrToInt(strRows);
+end;
+
+procedure TForm1.N2Click(Sender: TObject);
+var strCols:String;
+begin
+  strCols := inputbox('Количество столбцов', 'Укажите количество столбцов: ', '0');
+  Cols := StrToInt(strCols);
+end;
+
+procedure TForm1.Start1Click(Sender: TObject);
+var msg: String;
+  I: Integer;
+  j: Integer;
+begin
+  StringGrid1.Visible := false;
+  StringGrid2.Visible := false;
+  Randomize;
+  msg := '';
+  if Rows = 0 then
+    msg := 'Укажите количество строк';
+  if Cols = 0 then
+  begin
+    if msg.Length > 0 then
+      msg := msg + ', '
+    else
+      msg := 'Укажите количество ';
+    msg := msg + 'столбцов';
+  end;
+  Label1.Caption := msg;
+  if msg.Length = 0 then
+  begin
+    StringGrid1.RowCount := Rows;
+    StringGrid1.ColCount := Cols;
+    SetLength(Mas, Cols, Rows);
+    for i := 0 to Rows-1 do
+    begin
+      for j := 0 to Cols-1 do
+      begin
+        Mas[j, i] := Random();
+        StringGrid1.Cells[j,i] := IntToStr(Round(Mas[j, i]*RoundTo));
+      end;
+    end;
+    StringGrid1.Visible := true;
+  end;
+end;
+
+end.
